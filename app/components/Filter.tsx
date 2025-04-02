140d8ec1c14ab869d655fd83d2062dd647c78abe
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-02 21:54:53 +0100

    add buttons highpass and lowpass filter buttons

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -3,20 +3,19 @@
 import React, { useState } from "react";
 
 type FilterProps = {
-    state: "high-pass" | "low-pass";
-    onHighPassChange: (highPass: number) => void;
-    onLowPassChange: (lowPass: number) => void;
+    filterType: "none" | "highpass" | "lowpass";
+    onFilterChange: (type: "none" | "highpass" | "lowpass") => void;
 };
 
-export default function Filter({ state, onHighPassChange, onLowPassChange }: FilterProps) {
+export default function Filter({ filterType,onFilterChange  }: FilterProps) {
     const [highPassValue, setHighPassValue] = useState(1000);
         const [lowPassValue, setLowPassValue] = useState(50000);
-        
+
     return (
         
         <div>
-            <button>High Pass</button>
-            <button>Low Pass</button>
+            <button onClick={() => onFilterChange('none')}>High Pass</button>
+            <button onClick={() => onFilterChange('highpass')}>Low Pass</button>
         </div>
-    )
+    )   
 }
