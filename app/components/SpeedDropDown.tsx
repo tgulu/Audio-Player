df2b0aab684712c3429644a07286d394d7c92aef
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-03 14:14:43 +0100

    add more speed options

--- app/components/SpeedDropDown.tsx
+++ app/components/SpeedDropDown.tsx
@@ -1,28 +1,33 @@
-import React from "react"
-
+import React from "react";
 
 type SpeedDropDownProps = {
-    playbackRate: number;
-    onSpeedChange: (speed: number) => void;
+  playbackRate: number;
+  onSpeedChange: (speed: number) => void;
 };
 
 const SPEED_OPTIONS = [
-    { value: 0.5, label: "0.5x" },
-    { value: 1, label: "1x" },
-    { value: 2, label: "2x" },
+  { value: 0.5, label: "0.5x" },
+  { value: 1, label: "1x" },
+  { value: 1.5, label: "1.5x" },
+  { value: 2, label: "2x" },
 ];
 
-export default function SpeedDropDown({ playbackRate = 1, onSpeedChange }: SpeedDropDownProps) {
-    return (
-        <div>
-            <select value={playbackRate} onChange={(e) => onSpeedChange(Number(e.target.value))}>
-                {SPEED_OPTIONS.map((option) => (
-                    <option key={option.value} value={option.value}>
-                        {option.label}
-                    </option>
-                ))}
-            </select>
-        </div>
-
-    )
+export default function SpeedDropDown({
+  playbackRate = 1,
+  onSpeedChange,
+}: SpeedDropDownProps) {
+  return (
+    <div>
+      <select
+        value={playbackRate}
+        onChange={(e) => onSpeedChange(Number(e.target.value))}
+      >
+        {SPEED_OPTIONS.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
